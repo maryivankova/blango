7af264dd7d8188b14a34028b636dfa7aaae83161
Maryia Ivankov <mouse_iv@mail.ru>
2022-04-10 20:17:14 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+		'crispy_bootstrap5',
+		'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
